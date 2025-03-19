8d8fcd511ff3305ab5dd29691a69dd6bb7a3df1b
cati <joaopedroncps@gmail.com>
2025-03-19 15:12:52 -0300

    teste

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('oi');
+const uri = './assets/dados.json';
+var produtos = [
+    {produto: 'Boné'},
+    {produto: 'Camiseta'},
+    {produto: 'Sueter'},
+    {produto: 'Calça'},
+    {produto: 'Casaco'},
+    {produto: 'Luvas'},
+    {produto: 'Meias'},
+    {produto: 'Kit'}
+];
+fetch(uri)
+    .then(resp => resp.json())
+    .then(resp => { produtos = resp;
+    }).then(() =>mostrarCards())
+
+function mostrarCards(){
+    const main = document.querySelector('main');
+    produtos.forEach(p=>{
+        main.innerHTML += `
+        <div class="card">
+            <h3>${p.produto}</h3>
+            <img src="${p.imagem}" alt="${p.produto}">
+            <p>Preco: ${p.preco}</p>
+            <button>Detalhes</button>
+        </div>
+            `;
+    })
+}
